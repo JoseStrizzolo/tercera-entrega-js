8dea7e6bfc2add6c1981a39e50e3d3db74cf742d
Jose Strizzolo <jose.strizzolo@gmail.com>
2022-08-09 22:07:44 -0300

    tercera-entrega-js

---
+++ app.js
@@ -0,0 +1,29 @@
+//Variables
+const posicion = ["Maria", "Carlos", "Juan", "Pedro", "Federico", "Tomas"];{
+	console.log(posicion)
+}
+
+//Posiciones actuales           
+(posicion[0]);   
+(posicion[1]);
+(posicion[2]);
+(posicion[3]);
+(posicion[4]);
+(posicion[5]);
+ 
+//Cambios en posiciones
+posicion[2] = "Pedro";
+posicion[3] = "Juan";
+posicion.pop();
+posicion.push("Hugo", "Gustavo");
+posicion.unshift("Roco");
+
+//Posiciones actualizadas
+(posicion[0]); 
+(posicion[1]);
+(posicion[2]);
+(posicion[3]);
+(posicion[4]);
+(posicion[5]);
+(posicion[6]);
+(posicion[7]);
